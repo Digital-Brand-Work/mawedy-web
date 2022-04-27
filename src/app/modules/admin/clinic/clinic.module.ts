20af8f706e40bd15ba5ef2f005fb3eac46f4e200
Jamel Yassin <jamelyassin84@gmail.com>
2022-04-27 16:02:12 +0400

    update on clinic module

--- src/app/modules/admin/clinic/clinic.module.ts
+++ src/app/modules/admin/clinic/clinic.module.ts
@@ -1,27 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ClinicComponent } from './clinic.component';
-import { ClinicInformationComponent } from './clinic-information/clinic-information.component';
-import { ClinicServicesComponent } from './clinic-services/clinic-services.component';
-import { ClinicDepartmentAddComponent } from './clinic-services/modals/clinic-department-add/clinic-department-add.component';
-import { ClinicServicesAddComponent } from './clinic-services/modals/clinic-services-add/clinic-services-add.component';
-import { ClinicServicesEditComponent } from './clinic-services/modals/clinic-services-edit/clinic-services-edit.component';
-import { ClinicInformationMapComponent } from './clinic-information-map/clinic-information-map.component';
+import { SharedModule } from './../../../shared/shared.module'
+import { NgModule } from '@angular/core'
+import { ClinicComponent } from './clinic.component'
+import { ClinicInformationComponent } from './clinic-information/clinic-information.component'
+import { ClinicServicesComponent } from './clinic-services/clinic-services.component'
+import { ClinicDepartmentAddComponent } from './clinic-services/modals/clinic-department-add/clinic-department-add.component'
+import { ClinicServicesAddComponent } from './clinic-services/modals/clinic-services-add/clinic-services-add.component'
+import { ClinicServicesEditComponent } from './clinic-services/modals/clinic-services-edit/clinic-services-edit.component'
+import { ClinicInformationMapComponent } from './clinic-information-map/clinic-information-map.component'
 
-
+const components = [
+	ClinicComponent,
+	ClinicInformationComponent,
+	ClinicServicesComponent,
+	ClinicDepartmentAddComponent,
+	ClinicServicesAddComponent,
+	ClinicServicesEditComponent,
+	ClinicInformationMapComponent,
+]
 
 @NgModule({
-  declarations: [
-    ClinicComponent,
-    ClinicInformationComponent,
-    ClinicServicesComponent,
-    ClinicDepartmentAddComponent,
-    ClinicServicesAddComponent,
-    ClinicServicesEditComponent,
-    ClinicInformationMapComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+	declarations: [...components],
+	imports: [SharedModule],
+	exports: [...components],
 })
-export class ClinicModule { }
+export class ClinicModule {}
