b49a6e918b6892ce4f12c39efb487e016ad23845
Jamel Yassin <jamelyassin84@gmail.com>
2022-04-27 13:50:39 +0400

    Added Modules

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppToolbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
