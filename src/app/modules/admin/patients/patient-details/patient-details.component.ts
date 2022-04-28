9ac926f5da0a99750d8b0911129fb709d21c2a20
Jamel Yassin <jamelyassin84@gmail.com>
2022-04-28 17:17:00 +0400

    patient details filter

--- src/app/modules/admin/patients/patient-details/patient-details.component.ts
+++ src/app/modules/admin/patients/patient-details/patient-details.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { dbwAnimations } from '@digital_brand_work/animations/animation.api'
 
 @Component({
-  selector: 'patient-details',
-  templateUrl: './patient-details.component.html',
-  styleUrls: ['./patient-details.component.scss']
+	selector: 'patient-details',
+	templateUrl: './patient-details.component.html',
+	styleUrls: ['./patient-details.component.scss'],
+	animations: [...dbwAnimations],
 })
 export class PatientDetailsComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
