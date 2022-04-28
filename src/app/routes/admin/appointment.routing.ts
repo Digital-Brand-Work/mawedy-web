64407745303a4fc28768b2afe194b4ed6fdb0854
Jamel Yassin <jamelyassin84@gmail.com>
2022-04-28 10:51:55 +0400

    Appointment Routing

--- src/app/routes/admin/appointment.routing.ts
+++ src/app/routes/admin/appointment.routing.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router'
+import { AppointmentsDayCalendarComponent } from 'app/modules/admin/appointments/appointments/appointments-day-calendar/appointments-day-calendar.component'
+import { AppointmentsMonthCalendarComponent } from 'app/modules/admin/appointments/appointments/appointments-month-calendar/appointments-month-calendar.component'
+import { AppointmentsWeekCalendarComponent } from 'app/modules/admin/appointments/appointments/appointments-week-calendar/appointments-week-calendar.component'
+import { AppointmentsComponent } from 'app/modules/admin/appointments/appointments/appointments.component'
 
-export const appointmentRoutes: Routes = []
+export const appointmentRoutes: Routes = [
+	{
+		path: '',
+		component: AppointmentsComponent,
+		children: [
+			{
+				path: '',
+				redirectTo: 'month',
+				pathMatch: 'full',
+			},
+			{
+				path: 'month',
+				component: AppointmentsMonthCalendarComponent,
+			},
+			{
+				path: 'week',
+				component: AppointmentsWeekCalendarComponent,
+			},
+			{
+				path: 'day',
+				component: AppointmentsDayCalendarComponent,
+			},
+		],
+	},
+]
