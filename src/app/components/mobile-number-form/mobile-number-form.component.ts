64d5a83b2d6f0e8b0a2eedc6f8dd2c39982feade
Jamel Yassin <jamelyassin84@gmail.com>
2022-04-30 00:44:41 +0400

    Flags

--- src/app/components/mobile-number-form/mobile-number-form.component.ts
+++ src/app/components/mobile-number-form/mobile-number-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { countries } from 'app/mawedy-core/constants/country-codes.list'
 @Component({
-  selector: 'mobile-number-form',
-  templateUrl: './mobile-number-form.component.html',
-  styleUrls: ['./mobile-number-form.component.scss']
+	selector: 'mobile-number-form',
+	templateUrl: './mobile-number-form.component.html',
+	styleUrls: ['./mobile-number-form.component.scss'],
 })
 export class MobileNumberFormComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	countries = countries
+		.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1))
+		.reverse()
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	country_code = 'AE'
+
+	country_code1 = 'ae'
+
+	change(event) {
+		this.country_code1 = event.target.value.toLowerCase()
+	}
 }
