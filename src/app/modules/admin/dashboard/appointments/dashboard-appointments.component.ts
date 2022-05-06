c5a7a6a612c5fe25f322cc0ef2296e33211ef58a
unknown <jamelyassin84@gmail.com>
2022-05-06 05:42:15 -0700

    changes

--- src/app/modules/admin/dashboard/appointments/dashboard-appointments.component.ts
+++ src/app/modules/admin/dashboard/appointments/dashboard-appointments.component.ts
@@ -1,10 +1,12 @@
 import { SeoService } from '@digital_brand_work/services/seo.service'
 import { Component, OnInit } from '@angular/core'
+import { dbwAnimations } from '@digital_brand_work/animations/animation.api'
 
 @Component({
 	selector: 'dashboard-appointments',
 	templateUrl: './dashboard-appointments.component.html',
 	styleUrls: ['./dashboard-appointments.component.scss'],
+	animations: [...dbwAnimations],
 })
 export class DashboardAppointmentsComponent implements OnInit {
 	constructor(private seoService: SeoService) {}
