2c2a61f7edc16b863bac339b01a9e9cdc4b2b2a3
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-07 11:04:42 +0400

    Added Waiting Patient Table

--- src/app/modules/admin/dashboard/appointments/dashboard-appointment-table/dashboard-appointment-table.component.ts
+++ src/app/modules/admin/dashboard/appointments/dashboard-appointment-table/dashboard-appointment-table.component.ts
@@ -1,12 +1,16 @@
+import { dbwAnimations } from './../../../../../../@digital_brand_work/animations/animation.api'
 import { Component, OnInit } from '@angular/core'
 
 @Component({
 	selector: 'dashboard-appointment-table',
 	templateUrl: './dashboard-appointment-table.component.html',
 	styleUrls: ['./dashboard-appointment-table.component.scss'],
+	animations: [...dbwAnimations],
 })
 export class DashboardAppointmentTableComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit(): void {}
+
+	identity = (item: any) => item
 }
--- src/app/modules/admin/dashboard/waiting-patients/waiting-patients-table/waiting-patients-table.component.ts
+++ src/app/modules/admin/dashboard/waiting-patients/waiting-patients-table/waiting-patients-table.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { dbwAnimations } from '@digital_brand_work/animations/animation.api'
 
 @Component({
-  selector: 'waiting-patients-table',
-  templateUrl: './waiting-patients-table.component.html',
-  styleUrls: ['./waiting-patients-table.component.scss']
+	selector: 'waiting-patients-table',
+	templateUrl: './waiting-patients-table.component.html',
+	styleUrls: ['./waiting-patients-table.component.scss'],
+	animations: [...dbwAnimations],
 })
 export class WaitingPatientsTableComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	identity = (item: any) => item
 }
