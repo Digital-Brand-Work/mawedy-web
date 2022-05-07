682a79752242010090e669f3b6adcde93d50a516
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-07 15:05:23 +0400

    Added Subscription Summary

--- src/app/modules/admin/subscriptions/subscription-summary/subscription-summary.component.ts
+++ src/app/modules/admin/subscriptions/subscription-summary/subscription-summary.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { dbwAnimations } from '@digital_brand_work/animations/animation.api'
 
 @Component({
-  selector: 'subscription-summary',
-  templateUrl: './subscription-summary.component.html',
-  styleUrls: ['./subscription-summary.component.scss']
+	selector: 'subscription-summary',
+	templateUrl: './subscription-summary.component.html',
+	styleUrls: ['./subscription-summary.component.scss'],
+	animations: [...dbwAnimations],
 })
 export class SubscriptionSummaryComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
