bc4e05b0c2b62b83ab60e89714cd9e08e1628406
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-07 13:07:58 +0400

    Clinic Promotion Add and Edit

--- src/app/modules/admin/promotions/promotions-add/promotions-add.component.ts
+++ src/app/modules/admin/promotions/promotions-add/promotions-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, HostListener, OnInit } from '@angular/core'
 
 @Component({
 	selector: 'promotion-add',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core'
 })
 export class PromotionsAddComponent implements OnInit {
 	constructor() {}
+
+	@HostListener('document:keydown.escape')
+	onKeydownHandler() {
+		history.back()
+	}
 
 	ngOnInit(): void {}
 }
