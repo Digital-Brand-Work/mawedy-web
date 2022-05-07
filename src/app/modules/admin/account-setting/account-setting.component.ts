2319192a3deb0644c5468a3ce47269f440a13f23
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-07 18:12:16 +0400

    Changes

--- src/app/modules/admin/account-setting/account-setting.component.ts
+++ src/app/modules/admin/account-setting/account-setting.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { SeoService } from '@digital_brand_work/services/seo.service'
 
 @Component({
-  selector: 'account-setting',
-  templateUrl: './account-setting.component.html',
-  styleUrls: ['./account-setting.component.scss']
+	selector: 'account-setting',
+	templateUrl: './account-setting.component.html',
+	styleUrls: ['./account-setting.component.scss'],
 })
 export class AccountSettingComponent implements OnInit {
+	constructor(private seoService: SeoService) {}
 
-  constructor() { }
+	ngOnInit(): void {
+		this.seoService.generateTags({
+			title: `Aster Clinic `,
+		})
+		;(document.querySelector('html') as HTMLElement).style.position =
+			'fixed'
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnDestroy(): void {
+		;(document.querySelector('html') as HTMLElement).style.position =
+			'relative'
+	}
 }
