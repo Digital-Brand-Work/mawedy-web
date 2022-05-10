fb7bc90f5c31785bf55cad4860cd5219ec51ec4e
unknown <jamelyassin84@gmail.com>
2022-05-10 04:22:31 -0700

    Add Doctor

--- src/app/modules/admin/doctors/modals/doctor-add/working-schedule/working-schedule.component.ts
+++ src/app/modules/admin/doctors/modals/doctor-add/working-schedule/working-schedule.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
+import { WeekDay, weekDays } from 'app/mawedy-core/constants/app.constant'
 
 @Component({
-  selector: 'working-schedule',
-  templateUrl: './working-schedule.component.html',
-  styleUrls: ['./working-schedule.component.scss']
+	selector: 'working-schedule',
+	templateUrl: './working-schedule.component.html',
+	styleUrls: ['./working-schedule.component.scss'],
 })
 export class WorkingScheduleComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	@Output() onChangeSchedule = new EventEmitter()
 
-  ngOnInit(): void {
-  }
+	weekdays: string[] = weekDays
 
+	ngOnInit(): void {}
+
+	identity = (item: any) => item
+
+	shorten(day: WeekDay) {
+		const char = day.split('')
+
+		return `${char[0]}${char[1]}${char[2]}`
+	}
 }
