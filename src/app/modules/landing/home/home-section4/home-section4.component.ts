1ac8ce0f350ff7d1c88859b89356177fa8340143
unknown <jamelyassin84@gmail.com>
2022-05-13 04:04:52 -0700

    Changes

--- src/app/modules/landing/home/home-section4/home-section4.component.ts
+++ src/app/modules/landing/home/home-section4/home-section4.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core'
+import { dbwAnimations } from '@digital_brand_work/animations/animation.api'
 
 @Component({
 	selector: 'home-section4',
 	templateUrl: './home-section4.component.html',
 	styleUrls: ['./home-section4.component.scss'],
+	animations: [...dbwAnimations],
 })
 export class HomeSection4Component implements OnInit {
 	constructor() {}
