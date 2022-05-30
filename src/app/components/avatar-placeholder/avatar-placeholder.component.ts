b2c4f17bedfc241c22af60b4a92d45ebcb70d9a9
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-30 22:28:20 +0400

    Clinic Banner and Avatar Placeholder

--- src/app/components/avatar-placeholder/avatar-placeholder.component.ts
+++ src/app/components/avatar-placeholder/avatar-placeholder.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators'
+import { Component, OnInit } from '@angular/core'
+import { Clinic } from 'app/modules/admin/clinic/clinic.model'
+import { ClinicUserService } from 'app/modules/admin/clinic/clinic.service'
+import { BehaviorSubject, Subject } from 'rxjs'
 
 @Component({
-  selector: 'avatar-placeholder',
-  templateUrl: './avatar-placeholder.component.html',
-  styleUrls: ['./avatar-placeholder.component.scss']
+	selector: 'avatar-placeholder',
+	templateUrl: './avatar-placeholder.component.html',
+	styleUrls: ['./avatar-placeholder.component.scss'],
 })
 export class AvatarPlaceholderComponent implements OnInit {
+	constructor(private _clinicUserService: ClinicUserService) {}
 
-  constructor() { }
+	clinic$: BehaviorSubject<Clinic | null> = this._clinicUserService.clinic$
 
-  ngOnInit(): void {
-  }
+	unsubscribe$: Subject<any> = new Subject<any>()
 
+	initial: string = ''
+
+	ngOnInit(): void {
+		this.clinic$.pipe(takeUntil(this.unsubscribe$)).subscribe((clinic) => {
+			if (!clinic) {
+				return
+			}
+
+			if (clinic.name.split('').length === 1) {
+				return (this.initial = clinic.name)
+			}
+
+			const initials = clinic.name.split(' ')
+
+			if (initials.length === 1) {
+				const newInitial = initials[0].split('')
+
+				return (this.initial = newInitial[0] + newInitial[1])
+			}
+
+			this.initial = initials[0].charAt(0) + initials[1].charAt(0)
+		})
+	}
+
+	ngOnDestroy(): void {
+		this.unsubscribe$.next(null)
+
+		this.unsubscribe$.complete()
+	}
 }
--- src/app/components/banner-placeholder/banner-placeholder.component.ts
+++ src/app/components/banner-placeholder/banner-placeholder.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Clinic } from 'app/modules/admin/clinic/clinic.model'
+import { ClinicUserService } from 'app/modules/admin/clinic/clinic.service'
+import { BehaviorSubject } from 'rxjs'
 
 @Component({
-  selector: 'banner-placeholder',
-  templateUrl: './banner-placeholder.component.html',
-  styleUrls: ['./banner-placeholder.component.scss']
+	selector: 'banner-placeholder',
+	templateUrl: './banner-placeholder.component.html',
+	styleUrls: ['./banner-placeholder.component.scss'],
 })
 export class BannerPlaceholderComponent implements OnInit {
+	constructor(private _clinicUserService: ClinicUserService) {}
 
-  constructor() { }
+	clinic$: BehaviorSubject<Clinic | null> = this._clinicUserService.clinic$
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
