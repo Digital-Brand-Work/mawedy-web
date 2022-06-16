df136384c4aed483196c8e540b8669168fb18201
Jamel Yassin <jamelyassin84@gmail.com>
2022-06-16 19:41:23 +0400

    Talk to Us

--- src/app/modules/landing/talk-to-us/talk-to-us-section1/talk-to-us-section1.component.ts
+++ src/app/modules/landing/talk-to-us/talk-to-us-section1/talk-to-us-section1.component.ts
@@ -11,8 +11,11 @@ import { BreakPoint } from '@digital_brand_work/models/core.model'
 import { ScrollService } from '@digital_brand_work/services/scroll.service'
 import { SeoService } from '@digital_brand_work/services/seo.service'
 import { MediaService } from '@digital_brand_work/utilities/media.service'
+import { AlertState } from 'app/components/alert/alert.service'
 import { PHONE } from 'app/mawedy-core/constants/app.constant'
+import { ErrorHandlerService } from 'app/misc/error-handler.service'
 import { Observable } from 'rxjs'
+import { TalkToUsService } from './talk-to-us.service'
 
 @Component({
 	selector: 'talk-to-us-section1',
@@ -22,11 +25,14 @@ import { Observable } from 'rxjs'
 })
 export class TalkToUsSection1Component implements OnInit {
 	constructor(
-		private cdr: ChangeDetectorRef,
-		private _formBuilder: FormBuilder,
+		private _alert: AlertState,
 		private _seoService: SeoService,
+		private _cdr: ChangeDetectorRef,
+		private _formBuilder: FormBuilder,
 		private _mediaService: MediaService,
 		private _scrollService: ScrollService,
+		private _talkToUsService: TalkToUsService,
+		private _errorHandlerService: ErrorHandlerService,
 	) {
 		this._seoService.generateTags({
 			title: 'Mawedy | Talk to us Us',
@@ -43,9 +49,23 @@ export class TalkToUsSection1Component implements OnInit {
 	form: FormGroup = this._formBuilder.group({
 		name: ['', Validators.required],
 		email: ['', [Validators.required, Validators.email]],
-		phone: [''],
+		phone_number: [''],
+		phone_number_country_code: ['AE'],
 		message: ['', Validators.required],
 	})
+
+	errors: any = {}
+
+	isProcessing: boolean = false
+
+	handleMobileNumberChange(event: {
+		countryCode: string
+		phoneNumber: string
+	}): void {
+		this.form.get('phone_number')?.setValue(event.phoneNumber)
+
+		this.form.get('phone_country_code')?.setValue(event.countryCode)
+	}
 
 	ngOnInit(): void {}
 
@@ -54,10 +74,42 @@ export class TalkToUsSection1Component implements OnInit {
 
 		this._scrollService.scrollToTop()
 
-		this.cdr.detectChanges()
+		this._cdr.detectChanges()
 	}
 
 	ngOnDestroy(): void {
-		this.cdr.detach()
+		this._cdr.detach()
+	}
+
+	send() {
+		this.isProcessing = true
+
+		this._talkToUsService
+			.post(this.form.value)
+			.subscribe({
+				next: () => {
+					this.form.reset()
+
+					this._alert.add({
+						id: Math.floor(Math.random() * 100000000000).toString(),
+						title: `Message sent`,
+						message:
+							'Welcome have received your message and will contact you as soon as we can.',
+						type: 'success',
+					})
+				},
+				error: (http) => {
+					for (let key in http.error.errors) {
+						for (let errorKey in this.errors) {
+							if (key.includes(errorKey)) {
+								this.errors[errorKey] = true
+							}
+						}
+					}
+
+					this._errorHandlerService.handleError(http)
+				},
+			})
+			.add(() => (this.isProcessing = false))
 	}
 }
---
+++ src/app/modules/landing/talk-to-us/talk-to-us-section1/talk-to-us.service.ts
@@ -0,0 +1,11 @@
+import { BaseService } from './../../../../../@digital_brand_work/api/base.api'
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { NgxIndexedDBService } from 'ngx-indexed-db'
+
+@Injectable({ providedIn: 'root' })
+export class TalkToUsService extends BaseService<any> {
+	constructor(_http: HttpClient, _indexDbService: NgxIndexedDBService) {
+		super(_http, _indexDbService, 'v1/contact-us')
+	}
+}
