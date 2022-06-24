9145ff5bcc8e43820e273936ccd0dfce9057cdeb
Jamel Yassin <jamelyassin84@gmail.com>
2022-06-24 14:45:20 +0400

    Changes

--- src/app/modules/admin/dashboard/appointments/dashboard-appointment-filter/dashboard-appointment-filter.component.ts
+++ src/app/modules/admin/dashboard/appointments/dashboard-appointment-filter/dashboard-appointment-filter.component.ts
@@ -1,4 +1,13 @@
+import { AppointmentService } from 'app/modules/admin/appointments/appointment.service'
+import { empty, hasData } from 'app/mawedy-core/helpers'
 import { Component, OnInit, Output, EventEmitter } from '@angular/core'
+import { Department } from 'app/modules/admin/clinic/department/department.model'
+import { Observable, take } from 'rxjs'
+import { select, Store } from '@ngrx/store'
+import { MedicalService } from 'app/modules/admin/clinic/clinic-services/medical-service.model'
+import { Doctor } from 'app/modules/admin/doctors/doctor.model'
+import * as DashboardAppointmentActions from '../dashboard-appointment.actions'
+import * as dayjs from 'dayjs'
 
 @Component({
 	selector: 'dashboard-appointment-filter',
@@ -6,9 +15,93 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 	styleUrls: ['./dashboard-appointment-filter.component.scss'],
 })
 export class DashboardAppointmentFilterComponent implements OnInit {
-	constructor() {}
+	constructor(
+		private _store: Store<{
+			department: Department[]
+			medicalService: MedicalService[]
+		}>,
+		private _appointmentAPI: AppointmentService,
+	) {}
 
-	@Output() onFilter = new EventEmitter()
+	filter = {
+		status: 'Confirmed',
+		department_id: '',
+		doctor_id: '',
+		service_id: '',
+	}
+
+	departments$?: Observable<Department[]> = this._store.pipe(
+		select('department'),
+	)
+
+	medicalServices: MedicalService[] = []
+
+	doctors: Doctor[] = []
 
 	ngOnInit(): void {}
+
+	identity = (item: any): any => item
+
+	setMedicalServices(id: string) {
+		this.departments$.pipe(take(1)).subscribe((store: any) => {
+			const departments: Department[] = Object.values(store.entities)
+
+			const department = departments.find(
+				(department) => department.id === id,
+			)
+
+			if (!empty(department)) {
+				this.medicalServices = department.services
+
+				if (hasData(department.services)) {
+					this.doctors = department.services[0].doctors
+				}
+			}
+
+			this.filter.service_id = ''
+
+			this.filter.doctor_id = ''
+		})
+	}
+
+	onFilter() {
+		const search = {
+			waiting: 'false',
+			date: dayjs().toJSON(),
+			...this.filter,
+		}
+
+		for (let key in search) {
+			if (empty(search[key])) {
+				delete search[key]
+			}
+		}
+
+		this._appointmentAPI
+			.query(`?` + new URLSearchParams(search).toString())
+			.subscribe((appointments: any) => {
+				this._store.dispatch(
+					DashboardAppointmentActions.loadDashboardAppointments({
+						dashboardAppointments: appointments.data,
+					}),
+				)
+			})
+	}
+
+	onReset() {
+		const search = {
+			waiting: 'false',
+			date: dayjs().toJSON(),
+		}
+
+		this._appointmentAPI
+			.query(`?` + new URLSearchParams(search).toString())
+			.subscribe((appointments: any) => {
+				this._store.dispatch(
+					DashboardAppointmentActions.loadDashboardAppointments({
+						dashboardAppointments: appointments.data,
+					}),
+				)
+			})
+	}
 }
--- src/app/modules/admin/dashboard/waiting-patients/waiting-patients-filter/waiting-patients-filter.component.ts
+++ src/app/modules/admin/dashboard/waiting-patients/waiting-patients-filter/waiting-patients-filter.component.ts
@@ -1,14 +1,106 @@
+import { empty, hasData } from 'app/mawedy-core/helpers'
+import { AppointmentService } from 'app/modules/admin/appointments/appointment.service'
+import { Department } from './../../../clinic/department/department.model'
+import { MedicalService } from './../../../clinic/clinic-services/medical-service.model'
 import { Component, EventEmitter, OnInit, Output } from '@angular/core'
-
+import { select, Store } from '@ngrx/store'
+import * as WaitingPatientsActions from '../dashboard-waiting-patient.actions'
+import { Observable, take } from 'rxjs'
+import * as dayjs from 'dayjs'
+import { Doctor } from 'app/modules/admin/doctors/doctor.model'
 @Component({
 	selector: 'waiting-patients-filter',
 	templateUrl: './waiting-patients-filter.component.html',
 	styleUrls: ['./waiting-patients-filter.component.scss'],
 })
 export class WaitingPatientsFilterComponent implements OnInit {
-	constructor() {}
+	constructor(
+		private _store: Store<{
+			department: Department[]
+			medicalService: MedicalService[]
+		}>,
+		private _appointmentAPI: AppointmentService,
+	) {}
 
-	@Output() onFilter = new EventEmitter()
+	filter = {
+		status: 'Confirmed',
+		department_id: '',
+		doctor_id: '',
+		service_id: '',
+	}
+
+	departments$?: Observable<Department[]> = this._store.pipe(
+		select('department'),
+	)
+
+	medicalServices: MedicalService[] = []
+
+	doctors: Doctor[] = []
 
 	ngOnInit(): void {}
+
+	identity = (item: any): any => item
+
+	setMedicalServices(id: string) {
+		this.departments$.pipe(take(1)).subscribe((store: any) => {
+			const departments: Department[] = Object.values(store.entities)
+
+			const department = departments.find(
+				(department) => department.id === id,
+			)
+
+			if (!empty(department)) {
+				this.medicalServices = department.services
+
+				if (hasData(department.services)) {
+					this.doctors = department.services[0].doctors
+				}
+			}
+
+			this.filter.service_id = ''
+
+			this.filter.doctor_id = ''
+		})
+	}
+
+	onFilter() {
+		const search = {
+			waiting: 'true',
+			date: dayjs().toJSON(),
+			...this.filter,
+		}
+
+		for (let key in search) {
+			if (empty(search[key])) {
+				delete search[key]
+			}
+		}
+
+		this._appointmentAPI
+			.query(`?` + new URLSearchParams(search).toString())
+			.subscribe((appointments: any) => {
+				this._store.dispatch(
+					WaitingPatientsActions.loadDashboardWaitingPatients({
+						dashboardWaitingPatients: appointments.data,
+					}),
+				)
+			})
+	}
+
+	onReset() {
+		const search = {
+			waiting: 'true',
+			date: dayjs().toJSON(),
+		}
+
+		this._appointmentAPI
+			.query(`?` + new URLSearchParams(search).toString())
+			.subscribe((appointments: any) => {
+				this._store.dispatch(
+					WaitingPatientsActions.loadDashboardWaitingPatients({
+						dashboardWaitingPatients: appointments.data,
+					}),
+				)
+			})
+	}
 }
