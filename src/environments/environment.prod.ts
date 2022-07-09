033ca46dcb47b732615cbfd2038bc7e32346c507
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2022-07-09 12:59:42 +0800

    CHORE: update amplify config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
 	production: true,
-	// api: 'https://api.mawedy.ae/',
-	api: 'https://develop.api.mawedy.ae/',
+	api: 'https://api.mawedy.ae/',
 }
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,4 @@
 export const environment = {
 	production: false,
-	// api: '',
 	api: 'https://develop.api.mawedy.ae/',
 }
